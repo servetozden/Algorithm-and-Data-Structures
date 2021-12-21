07e57c7dd13541ccc6c2a4c9f78c9613dc6e4de0
Servet Serhat Özden <servetozden50@hotmail.com>
2021-12-22 01:10:52 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 public class BinarySearch {
 
     public static void main(String[] args) {
+        
+        //Binary Search is an algorithm for finding a specific value in a sorted array. At each step in this technique, it is checked whether the sought value is equal to the middle value of the array.
         int arr[] = {10,5,20,30,40,50};
         int key = 5;
         int result = Arrays.binarySearch(arr,key);
