a3a73448ee004f66693349dbef7d10ae05baa938
Servet Serhat Özden <servetozden50@hotmail.com>
2022-01-02 13:44:10 +0300

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+package Algorithm;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        Integer[] data = {1,9,6,2,3,};
+        sort(data);
+    }
+
+    static void sort(Integer[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+
+    }
+
+}
