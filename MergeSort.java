d6d362974116313a46005f7e5c5dc225fd113580
Servet Serhat Özden <servetozden50@hotmail.com>
2022-01-11 18:53:27 +0300

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -6,6 +6,9 @@ public class MergeSort {
          sort(data);
     }
 
+    //Merge sort is one of the most efficient sorting algorithms.
+    //It works on the principle of Divide and Conquer. 
+    //Merge sort repeatedly breaks down a list into several sublists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list.
 
 
     public static void sort(Comparable[] a) {
