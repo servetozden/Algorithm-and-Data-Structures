9fdbcf539dd2482b539088e968fd0d22d8d696c4
Servet Serhat Özden <servetozden50@hotmail.com>
2022-01-19 23:39:58 +0300

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -1,5 +1,8 @@
 package Algorithm;
 
+//Radix sort is a sorting algorithm that sorts the elements by 
+//first grouping the individual digits of the same place value. 
+//Then, sort the elements according to their increasing/decreasing order.
 
     public class RadixSort {
 
